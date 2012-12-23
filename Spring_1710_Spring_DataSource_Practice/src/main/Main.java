e4fd31e2dbfa805575aa15fa5b870b22c9cacfcc
zhouwei <leonzhouwei@gmail.com>
2012-12-23 14:08:42 +0800

    Spring_1710 now has a better file hierarchy which I think is the best until now

---
+++ Spring_1710_Spring_DataSource_Practice/src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import main.java.com.bjsxt.model.Account;
+import main.java.com.bjsxt.service.AccountService;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		String[] configLocations = { "classpath:beans.xml" };
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(configLocations);
+		
+		AccountService service = (AccountService)ctx.getBean("accountService");
+		System.out.println(service.getClass());
+		Account account = new Account();
+		String name = "zhangsan";
+		String password = name;
+		account.setName(name);
+		account.setPassword(password);
+		service.add(account);
+	}
+}
---
+++ Spring_1710_Spring_DataSource_Practice/src/main/java/com/bjsxt/model/Account.java
@@ -0,0 +1,24 @@
+package main.java.com.bjsxt.model;
+
+public class Account {
+	
+	private String name;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
